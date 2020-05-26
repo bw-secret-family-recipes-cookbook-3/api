99e354e9853c9e319015d90781a9667dafae1673
mgomri <mel77gomri@gmail.com>
2020-05-26 03:36:07 -0400

    more testing

--- index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app = require('./api/server');
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`\n\n*** Server is listening on port ${port} ***\n`);
-});
